2632e4bde1ce52c658d8a480aabcef3d2176560f
Gerardo Gallegos <puxerop@gmial.com>
2016-04-26 21:09:58 -0500

    It's update de Code Component to Trik

--- src/components/Trik/RenderDisplay/Code/Code.jsx
+++ src/components/Trik/RenderDisplay/Code/Code.jsx
@@ -1,25 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+import { connect } from 'react-redux'
 
-class Instructions extends Component {
-  constructor(props) {
+// styles
+import './Code.scss'
+
+// Editor ACE
+import brace from 'brace'
+import AceEditor from 'react-ace'
+import 'brace/mode/javascript'
+import 'brace/mode/css'
+import 'brace/mode/html'
+import 'brace/theme/monokai'
+
+
+class CodeClient extends Component {
+  constructor(props){
     super(props)
+    this.onChangeCode = this.onChangeCode.bind(this)
+  }
+
+  onChangeCode(newCodeValue){
+    // Action to do
   }
 
   render() {
-    return(
+
+    return (
       <div className="Code-panel">
         <div className="Code-panel__top">
-          <span className="Code-panel__top__fileName">{ this.props.code.fileName }</span>
+          <div className='Code-panel__top__ball'></div>
+          <div className='Code-panel__top__ball'></div>
+          <div className='Code-panel__top__ball'></div>
+          <span className="Code-panel__top__fileName">
+            { this.props.code.fileName }
+          </span>
         </div>
-        <pre className="Code-panel__code">
-          {
-            this.props.code.text
-          }
-        </pre>
+        <div className="Code-panel__code">
+          <AceEditor
+            showGutter={false}
+            mode={this.props.code.fileType}
+            theme="monokai"
+            name="AceCodeEditorClient"
+            fontSize={20}
+            width="100%"
+            height="90%"
+            value={this.props.code.text}
+            editorProps={
+              {
+                $blockScrolling: true,
+                mode: 'javascript'
+              }
+            }
+            enableBasicAutocompletion={true}
+            enableLiveAutocompletion={true}
+          />
+        </div>
       </div>
     )
   }
-
 }
 
-export default Instructions
+
+export default CodeClient
